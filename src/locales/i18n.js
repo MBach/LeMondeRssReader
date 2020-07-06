2e02ba89c29d91dacf6938fbe05c76af9643f4c1
Matthieu BACHELIER <bachelier.matthieu@gmail.com>
2020-07-06 11:32:13 +0200

    Fix fallback

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -6,24 +6,31 @@ i18n.locale = 'fr'
 i18n.fallbacks = true
 
 export const loadLocale = async () => {
+  let hasMatchingLang = false
   for (const locale of RNLocalize.getLocales()) {
-    if (i18n.translations[locale.languageCode] !== null) {
+    if (i18n.translations[locale.languageCode]) {
       i18n.locale = locale.languageCode
+      hasMatchingLang = true
       switch (locale.languageCode) {
         case 'en':
-          import('./en.json').then(en => {
+          import('./en.json').then((en) => {
             i18n.translations = { en }
           })
           break
         default:
         case 'fr':
-          import('./fr.json').then(fr => {
+          import('./fr.json').then((fr) => {
             i18n.translations = { fr }
           })
           break
       }
       break
     }
+  }
+  if (!hasMatchingLang) {
+    import('./fr.json').then((fr) => {
+      i18n.translations = { fr }
+    })
   }
 }
 
